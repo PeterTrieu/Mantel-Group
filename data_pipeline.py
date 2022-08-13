b81a65b245ce1a9fe5924e078d2e8ebe8e2ad37f
PeterTrieu <45331712+PeterTrieu@users.noreply.github.com>
2022-08-13 11:39:58 +1000

    Saved the results in a CSV format

--- data_pipeline.py
+++ data_pipeline.py
@@ -4,7 +4,10 @@
 import json
 import os
 from collections import Counter
+import csv
+from datetime import date
 
+# ====================== EXTRACTING ===============================
 # Extracting all files within the weather_data folder
 path = "weather_data/"
 files = os.listdir(path)
@@ -15,13 +18,13 @@ with open('weather_data/'+files[0]) as f:
     # Returning JSON object as a dictionary
     data = json.load(f)
 
-# Create array based on the number of array of objects in JSON file to store the temperature and weather text
+# Create array based on the number of array of objects in JSON file to store the temperature, weather text and three common weather text
 temp_array = [0]*len(data)
 weather_text_array = ["No Data"]*len(data)
+three_common_array = ["No Data"]*3
 
 # Extract the city name
-city_name = data[0]["city"].title() # Capitalise the first letter of word
-print(city_name)
+city_name = data[0]["city"].strip().title() # Remove leading and trailing white spaces and capitalise the first letter of word
 
 # Iterating through the JSON list
 for i in range(len(data)):
@@ -29,11 +32,33 @@ for i in range(len(data)):
     temp_array[i] = data[i]["Temperature"]["Metric"]["Value"] # Assume that we are using Metric Value
     weather_text_array[i] = data[i]["WeatherText"].title() # Capitalise the first letter of word
 
+# Closing the JSON file
+f.close()
+
+# ====================== TRANFORMING ===============================
 # Calculating the average temperature
-average = round(sum(temp_array)/len(temp_array),1) # Round to nearest 1 decimal place
+average_temp = round(sum(temp_array)/len(temp_array),1) # Round to nearest 1 decimal place
 
 # Calculating the top three most common weather text
 weather_text_counter = Counter(weather_text_array) # Convert array of weather text to Counter class
 three_common = weather_text_counter.most_common(3) # Ordered alphabetically
-print(weather_text_array)
-print(three_common)
+
+# Iterating to retrieve only the text value of the three common weather text
+for j in range(len(three_common)):
+    three_common_array[j] = three_common[j][0].strip() # Remove leading and trailing white spaces
+
+# ====================== LOADING ===============================
+# Create the file and open it in write mode
+f = open('weather_results/'+str(date.today().strftime("%d-%m-%Y"))+'.csv', 'w', newline='') 
+
+# Create the csv write
+writer = csv.writer(f)
+
+# write the header row to the csv file
+writer.writerow(["City","Average Temperature in Celcius","Top Three Most Common Weather Text"])
+
+# write the header row to the csv file
+writer.writerow([city_name,average_temp,three_common_array]) 
+
+# close the file
+f.close()
