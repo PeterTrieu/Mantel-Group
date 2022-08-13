040a9daeacfde470ce5535f3b2caaada01a5a344
PeterTrieu <45331712+PeterTrieu@users.noreply.github.com>
2022-08-13 12:17:24 +1000

    Updated the formatting of the results that is produced in CSV format

--- data_pipeline.py
+++ data_pipeline.py
@@ -5,7 +5,21 @@ import json
 import os
 from collections import Counter
 import csv
-from datetime import date
+from datetime import datetime
+
+# Creating the csv file
+# Create the file and open it in write mode only if file doesn't exist
+if(os.path.exists('weather_results/weather_results.csv') == False):
+    f = open('weather_results/weather_results.csv', 'w', newline='') 
+
+    # Create the csv write
+    writer = csv.writer(f)
+
+    # Write the header row to the csv file
+    writer.writerow(["Start Date","End Date","Start Time","End Time","City","Average Temperature in Celcius","Top Three Most Common Weather Text"])
+
+    # close the file
+    f.close()
 
 # ====================== EXTRACTING ===============================
 # Extracting all files within the weather_data folder
@@ -23,7 +37,17 @@ temp_array = [0]*len(data)
 weather_text_array = ["No Data"]*len(data)
 three_common_array = ["No Data"]*3
 
-# Extract the city name
+# Retrieve the start datetime and end datetime of data
+start_datetime = data[len(data)-1]["LocalObservationDateTime"]
+end_datetime = data[0]["LocalObservationDateTime"]
+
+# Extract the start date, start time, end date and end time from the start datetime and end datetime
+start_date = datetime.strptime(start_datetime, '%Y-%m-%dT%H:%M:%S%z').date()
+end_date = datetime.strptime(end_datetime, '%Y-%m-%dT%H:%M:%S%z').date()
+start_time = datetime.strptime(start_datetime, '%Y-%m-%dT%H:%M:%S%z').time()
+end_time = datetime.strptime(end_datetime, '%Y-%m-%dT%H:%M:%S%z').time()
+
+# Retrieve the city name
 city_name = data[0]["city"].strip().title() # Remove leading and trailing white spaces and capitalise the first letter of word
 
 # Iterating through the JSON list
@@ -49,16 +73,13 @@ for j in range(len(three_common)):
 
 # ====================== LOADING ===============================
 # Create the file and open it in write mode
-f = open('weather_results/'+str(date.today().strftime("%d-%m-%Y"))+'.csv', 'w', newline='') 
+f = open('weather_results/weather_results.csv', 'a', newline='') 
 
 # Create the csv write
 writer = csv.writer(f)
 
-# write the header row to the csv file
-writer.writerow(["City","Average Temperature in Celcius","Top Three Most Common Weather Text"])
-
-# write the header row to the csv file
-writer.writerow([city_name,average_temp,three_common_array]) 
+# Write the row of data
+writer.writerow([start_date,end_date,start_time,end_time,city_name,average_temp,three_common_array]) 
 
 # close the file
 f.close()
